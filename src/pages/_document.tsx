4257fe1b724b27ead5c34695de78488c1caa9da9
Luis-Felipe-N <luisj2felipe09@gmail.com>
2021-11-24 23:35:46 -0300

    fixing erro in document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head/>
         <head>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
             <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@300;400;500;600&family=Bellefair&display=swap" rel="stylesheet" />
         </head>
         <body>
